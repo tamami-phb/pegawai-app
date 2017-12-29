8c2cb3f67d20a90b4839568d24743b3c7933b693
tamami <tamami.oka@gmail.com>
2017-12-29 16:29:04 +0700

    init file daftar-pegawai.html

---
+++ src/main/java/lab/phb/pegawaiapp/config/WebConfig.java
@@ -0,0 +1,16 @@
+package lab.phb.pegawaiapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry reg) {
+        reg.addViewController("/")
+            .setViewName("daftar-pegawai");
+    }
+
+}
