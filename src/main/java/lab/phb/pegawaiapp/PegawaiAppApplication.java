c4ad1ad558c37567a6b2269e1c7de8b80c264ee3
tamami <tamami.oka@gmail.com>
2017-12-29 16:05:10 +0700

    init commit

---
+++ src/main/java/lab/phb/pegawaiapp/PegawaiAppApplication.java
@@ -0,0 +1,12 @@
+package lab.phb.pegawaiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegawaiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegawaiAppApplication.class, args);
+	}
+}
